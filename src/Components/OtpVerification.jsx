37f03cd370af8d913776395ffc51bf355245cab5
ashutosshukla <90246033+ashutosshukla@users.noreply.github.com>
2024-10-19 17:45:11 +0530

    message

--- src/Components/OtpVerification.jsx
+++ src/Components/OtpVerification.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Card from './ui/Card';
+import Card from './ui/Card.jsx';
 import CardContent from './ui/CardContent';
 import Input from './ui/Input'
 import Button  from './ui/Button'
